5454137c7fe4322e72abe9d33170e2d225a3196b
William Alfred Gazal Júnior <williamjunior67@gmail.com>
2020-09-23 10:12:57 -0300

    Add files via upload

---
+++ Gft-ex4-william/src/gft/ex4/william/GftEx4William.java
@@ -0,0 +1,19 @@
+
+package gft.ex4.william;
+import java.util.Scanner;
+
+public class GftEx4William {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int i;
+        
+        int n = entrada.nextInt();
+        
+        for(i = 0; i<n; i++){
+            System.out.print("incomoda ");
+        }
+        System.out.print("muito mais.\n");
+    }
+    
+}
